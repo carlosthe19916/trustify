613b0780541dc9bb60836f0552147ba882b9a423
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-11-20 15:27:17 -0500

    feat: add codespaces
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

--- .devcontainer/full/initializeCommand.sh
+++ .devcontainer/full/initializeCommand.sh
@@ -11,9 +11,6 @@ if [ "${CODESPACES}" = "true" ]; then
     KC_HOSTNAME="https://${CODESPACE_NAME}-9090.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
     UI_ISSUER_URL="https://${CODESPACE_NAME}-9090.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}/realms/trustify"
 
-    echo "carlos"
-    pwd
-
-    echo "KC_HOSTNAME=\"$KC_HOSTNAME\"" >> .devcontainer/full/.env
-    echo "UI_ISSUER_URL=\"$UI_ISSUER_URL\"" >> .devcontainer/full/.env
+    echo "KC_HOSTNAME=$KC_HOSTNAME" >> .devcontainer/full/.env
+    echo "UI_ISSUER_URL=$UI_ISSUER_URL" >> .devcontainer/full/.env
 fi
