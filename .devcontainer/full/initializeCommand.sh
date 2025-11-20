6d8e4d33d37e7432e018e5acf3a0363a671132db
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-11-20 11:54:07 -0500

    feat: add codespaces
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

--- .devcontainer/full/initializeCommand.sh
+++ .devcontainer/full/initializeCommand.sh
@@ -4,3 +4,11 @@
 # just before the container is launched but after the container is created
 
 echo "devcontainerID ${1}"
+
+KC_HOSTNAME="${CODESPACE_NAME:+${CODESPACE_NAME}-9090.app.github.dev}"
+UI_ISSUER_URL="${CODESPACE_NAME:+${CODESPACE_NAME}-9090.app.github.dev/realms/trustify}"
+
+{
+  echo "KC_HOSTNAME=\"$KC_HOSTNAME\""
+  echo "UI_ISSUER_URL=\"$UI_ISSUER_URL\""
+} > .env
